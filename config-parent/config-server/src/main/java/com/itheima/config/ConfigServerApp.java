3a2f4dc87618dcf1d3b3169a0e6b58046707d9f8
Jay Zhang <godcola@163.com>
2021-07-18 16:14:54 +0800

    first commit

---
+++ config-parent/config-server/src/main/java/com/itheima/config/ConfigServerApp.java
@@ -0,0 +1,15 @@
+package com.itheima.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer //启用ConfigServer的功能
+@EnableEurekaClient
+public class ConfigServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApp.class,args);
+    }
+}
